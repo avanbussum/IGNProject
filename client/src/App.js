b499ac173d2c362346669b7cbd36831dc9bbc539
Alex Van Bussum <avanbussum@gmail.com>
2022-04-30 20:57:47 -0700

    Final commit w/ a few loading issues

--- client/src/App.js
+++ client/src/App.js
@@ -3,96 +3,90 @@ import { useState, useEffect, useRef } from "react";
 import Axios from "axios";
 import React from "react";
 
-const genreList = ['Drama','Horror','Action','Sci-Fi','Fantasy','Animation','Crime','Anime']
-const mediaTypeList = ['Movie','Show','Comic','Game']
+const genreList = [
+  "Drama",
+  "Horror",
+  "Action",
+  "Sci-Fi",
+  "Fantasy",
+  "Animation",
+  "Crime",
+  "Anime",
+];
+const mediaTypeList = ["Movie", "Show", "Comic", "Game"];
 
 function App() {
-  const [name, setName] = useState("");
-  const [id, setId] = useState(0);
-  const [review_url, setReview_Url] = useState("");
-  const [short_description, setShort_description] = useState("");
-  const [genres, setGenre] = useState("");
-  const [media_type, setMediaType] = useState("");
-
-  
   const [searchMediaType, setSearchMediaType] = useState("All Media");
-  const [searchGenre, setSearchGenre] = useState("All Genres");
+  const [searchGenre, setSearchGenre] = useState("Fantasy");
   const [mediaList, setMediaList] = useState([]);
-  
 
+  // Get all media dat from database call to backend server
   useEffect(() => {
-    Axios.get("http://localhost:3004/medias").then(
-      (response) => {
-        console.log(response.data);
-        setMediaList(response.data);
-      }
-    );
-  },[]);
-  if(mediaList.length == 0){
-    return <div>loading</div>;
-  }
-
-  const getMedia = () => {
     Axios.get("http://localhost:3004/medias").then((response) => {
       console.log(response.data);
       setMediaList(response.data);
     });
-  };
-
-  const getGenre = () => {
-    Axios.get("http://localhost:3004/genrefinder/" + searchGenre).then(
-      (response) => {
-        console.log(response.data);
-        setMediaList(response.data);
-      });
-  };
-
-  const getMediaType = () => {
-    Axios.get("http://localhost:3004/typefinder/" + searchMediaType).then(
-      (response) => {
-        console.log(response.data);
-        setMediaList(response.data);
-      });
-  };
+  }, []);
+  if (mediaList.length === 0) {
+    return <div>loading</div>;
+  }
 
   return (
+    // Passing params to dropdowns and cards
     <div className=".App">
-      <TailwindButton setSearchGenre={setSearchGenre} setSearchMediaType={setSearchMediaType}>Show All Entertainment</TailwindButton>
+      <SearchAllButton
+        setSearchGenre={setSearchGenre}
+        setSearchMediaType={setSearchMediaType}
+      >
+        Show All Entertainment
+      </SearchAllButton>
       <div className="flex px-5 justify-center">
         <div className="flex items-center justify-center ">
           <div className="flex border-gray-200 rounded-lg gap-x-96">
-          
-            <GenreDropDown setSearchGenre={setSearchGenre} searchGenre={searchGenre}></GenreDropDown>  
-            <MediaDropDown setSearchMediaType={setSearchMediaType} searchMediaType={searchMediaType}></MediaDropDown>
-            
+            <GenreDropDown
+              setSearchGenre={setSearchGenre}
+              searchGenre={searchGenre}
+            ></GenreDropDown>
+            <MediaDropDown
+              setSearchMediaType={setSearchMediaType}
+              searchMediaType={searchMediaType}
+            ></MediaDropDown>
           </div>
         </div>
       </div>
       <div className="p-4 align-middle">
         <div className="mx-2 lg:w-[80%] lg:m-auto">
-        <img src="https://1000logos.net/wp-content/uploads/2021/05/IGN-Logo.png" className="justify-center w-22 h-14"></img>
+          <img
+            src="https://1000logos.net/wp-content/uploads/2021/05/IGN-Logo.png"
+            alt="lorem ipsum"
+            className="justify-center w-22 h-14"
+          ></img>
           <h3 className="bg-red-500 font-bold text-white my-5 lg:my-0 px-4 py-0.5">
             Recent Reviews
           </h3>
-          
+
           <div className="lg:grid grid-cols-4 grid-row-2">
-            <Items mediaList={mediaList} searchGenre={searchGenre} searchMediaType={searchMediaType}></Items>
+            <Items
+              mediaList={mediaList}
+              searchGenre={searchGenre}
+              searchMediaType={searchMediaType}
+            ></Items>
           </div>
         </div>
       </div>
     </div>
-    
   );
 }
 
-function TailwindButton(props) {
-  const {setSearchGenre, setSearchMediaType} = props;
-  console.log({setSearchGenre})
+// Get all Media (acts as a refresher from genres)
+function SearchAllButton(props) {
+  const { setSearchGenre, setSearchMediaType } = props;
+  console.log({ setSearchGenre });
   return (
     <button
-      onClick={() => { 
-        setSearchGenre('All Genres')
-        setSearchMediaType('All Media')
+      onClick={() => {
+        setSearchGenre("All Genres");
+        setSearchMediaType("All Media");
       }}
       className="bg-red-500 text-white font-medium px-4 py-2 m-5 rounded hover:bg-red-600"
     >
@@ -102,8 +96,9 @@ function TailwindButton(props) {
   );
 }
 
+// Set Selected Genre
 function GenreDropDown(props) {
-  const {setSearchGenre, searchGenre} = props
+  const { setSearchGenre, searchGenre } = props;
   return (
     <div className="relative inline-flex gap-x-4">
       <svg
@@ -117,50 +112,62 @@ function GenreDropDown(props) {
           fillRule="nonzero"
         />
       </svg>
-      
+
       <select
-        onChange= {(e) => setSearchGenre(e.target.value)} value={searchGenre}
+        onChange={(e) => setSearchGenre(e.target.value)}
+        value={searchGenre}
         className="border border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 cursor-pointer focus:outline-none appearance-none"
       >
         <option value="All Genres">All Genres</option>
-        {genreList.map((specificgenre, i) => {
-          return (
-            <option key={i}>
-              {specificgenre}
-            </option>
-          );
+        {genreList.map((specificGenre, i) => {
+          return <option key={i}>{specificGenre}</option>;
         })}
       </select>
-      
-    </div>      
+    </div>
   );
 }
 
-function Items (props) {
-  const {mediaList, searchGenre, searchMediaType} = props
-  console.log({searchGenre, searchMediaType})
-
-  const items = mediaList.map(
-    (val) => ({...val, genres:val.genres.replace('{','').replace('}','')})
-  ).filter(item => {
-
-    const hasGenre = searchGenre == 'All Genres' ? true : item.genres.split(',').includes(searchGenre)
-    const hasMediaType = searchMediaType == 'All Media' ? true : item.media_type.includes(searchMediaType)
-
-    console.log({item,hasGenre,hasMediaType,searchGenre,searchMediaType,split:item.genres.split(',').join(',')})
-
-    return hasGenre && hasMediaType
-  }).map((val) => (
-      <div key = {val.id} className="flex flex-row lg:flex-col mx-5 lg:mx-2 lg:mt-0 my-5 pb-5 items-center md:items-start lg:bg-white border-b-2 border-red-300 relative shadow-lg hover:bg-gray-50 hover:drop-shadow-lg">
+function Items(props) {
+  const { mediaList, searchGenre, searchMediaType } = props;
+  // Items is returned in card format
+  const items = mediaList
+    // Remove Parentheses (do this in the backend)
+    .map((val) => ({
+      ...val,
+      genres: val.genres.replace("{", "").replace("}", ""),
+    }))
+    // Filter for multiple genres
+    .filter((item) => {
+      const hasGenre =
+        searchGenre === "All Genres"
+          ? true
+          : item.genres.split(",").includes(searchGenre);
+      const hasMediaType =
+        searchMediaType === "All Media"
+          ? true
+          : item.media_type.includes(searchMediaType);
+      return hasGenre && hasMediaType;
+    })
+    // Map genre and mediaType call to cards
+    .map((val) => (
+      <div
+        key={val.id}
+        className="flex flex-row lg:flex-col mx-5 lg:mx-2 lg:mt-0 my-5 pb-5 items-center md:items-start lg:bg-white border-b-2 border-red-300 relative shadow-lg hover:bg-gray-50 hover:drop-shadow-lg"
+      >
         <div className="bg-gray-400 text-gray-50 text-xs uppercase font-bold rounded-full p-2 absolute top-3 ml-2 mt-0">
           <span>ID: {val.id}</span>
         </div>
-        <a href={val.review_url} className="hover:cursor-pointer hover:drop-shadow-lg focus:ring focus:ring-violet-300">
+        <a
+          href={val.review_url}
+          className="hover:cursor-pointer hover:drop-shadow-lg focus:ring focus:ring-violet-300"
+        >
           <Image rev={val.review_url}></Image>
         </a>
         <div className="md:flex md:flex-col lg:px-3 gap-y-3 ">
-          <h6 className="font-bold text-lg items-justify-center py-4">{val.name}</h6>
-          
+          <h6 className="font-bold text-lg items-justify-center py-4">
+            {val.name}
+          </h6>
+
           <p className="hidden md:block my-2 lg:my-0">
             <span className="font-semibold">Media Type: </span>
             {val.media_type}
@@ -175,16 +182,13 @@ function Items (props) {
           </p>
         </div>
       </div>
-    )
-  );
-  return(
-    <>{items}</>
-  );
+    ));
+  return <>{items}</>;
 }
 
+//Similar to Genre Drop Down filtered in Items
 function MediaDropDown(props) {
-  const {setSearchMediaType, searchMediaType} = props
-
+  const { setSearchMediaType, searchMediaType } = props;
   return (
     <div className="relative inline-flex gap-x-4">
       <svg
@@ -199,90 +203,97 @@ function MediaDropDown(props) {
         />
       </svg>
       <select
-        onChange= {(e) => setSearchMediaType(e.target.value)} value={searchMediaType}
+        onChange={(e) => setSearchMediaType(e.target.value)}
+        value={searchMediaType}
         className="border border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 cursor-pointer focus:outline-none appearance-none"
       >
         <option value="All Media">All Media</option>
         {mediaTypeList.map((specmedia, i) => {
-          return (
-            <option key={i}>
-              {specmedia}
-            </option>
-          );
+          return <option key={i}>{specmedia}</option>;
         })}
       </select>
     </div>
   );
 }
 
+function Loading() {
+  return (
+    <div className="m-24">
+      <svg
+        role="status"
+        className="  w-14 h-14 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+    </div>
+  );
+}
+
+// Image webscraper from IGN review Articles
+// Load time is slow
 function Image(props) {
   const [imageURL, setImageURL] = useState("");
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     Axios.get("http://localhost:3004/reviewfind/" + props.rev).then(
       (response) => {
-        setImageURL(response.data.meta.image);
-        setLoading(false);
+        setImageURL(response.data);
+        setIsLoading(false);
       }
     );
-  },[]);
-  
+  }, []);
+
   if (isLoading) {
-    return <div className="m-24"><svg role="status" className="  w-14 h-14 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-    </svg></div>;
+    return <Loading></Loading>;
   }
 
-  return(
+  return (
     <img
       className="rounded-full object-contain hover:object-scale-down mt-2"
-      src= {imageURL}
+      src={imageURL}
       alt="Lorem Impsum"
     ></img>
-  )
+  );
 }
 
 export default App;
 
-//ADD MEDIA FUNCTION
+// Extraneous API calls utilized before new method of filtering
+// Kept in case new API Call Methods to the database need to be utilized
+/*
+const getMedia = () => {
+    Axios.get("http://localhost:3004/medias").then((response) => {
+      console.log(response.data);
+      setMediaList(response.data);
+    });
+  };
 
-// const addMedia = () => {
-//   Axios.post("http://localhost:3004/create", {
-//     id: id,
-//     searchMediaType: searchMediaType,
-//     name: name,
-//     short_description: short_description,
-//     genres: genres,
-//   }).then(() => {
-//     setEmployeeList([
-//       ...employeeList,
-//       {
-//         name: name,
-//         age: age,
-//         country: country,
-//         position: position,
-//         wage: wage,
-//       },
-//     ]);
-//   });
-// };
+  const getGenre = () => {
+    Axios.get("http://localhost:3004/genrefinder/" + searchGenre).then(
+      (response) => {
+        console.log(response.data);
+        setMediaList(response.data);
+      }
+    );
+  };
 
-//BASIC DISPLAY
-
-// {
-//   mediaList.map((val, key) => {
-//     return (
-//       <div className="media">
-//         <div>
-//           <h3>ID: {val.id}</h3>
-//           <h3>Name: {val.name}</h3>
-//           <h3>Media Type: {val.searchMediaType}</h3>
-//           <h3>Short Decription: {val.short_description}</h3>
-//           <h3>Genre: {val.genres}</h3>
-//         </div>
-//       </div>
-//     );
-//   })
-// }
+  const getMediaType = () => {
+    Axios.get("http://localhost:3004/typefinder/" + searchMediaType).then(
+      (response) => {
+        console.log(response.data);
+        setMediaList(response.data);
+      }
+    );
+  };
+*/
--- server/index.js
+++ server/index.js
@@ -7,8 +7,6 @@ const cors = require('cors');
 const axios = require('axios');
 
 app.use(cors());
-// app.use(axios());
-
 app.use(morgan('combined'));
 app.use(express.json());
 
@@ -30,35 +28,10 @@ db.connect((err) => {
     }
 });
 
-// // Insert media
-// app.post("/create", (req, res) => {
-    
-//     const id = req.body.id;
-//     const media_type = req.body.media_type;
-//     const name = req.body.name;
-//     const short_name = req.body.short_name;
-//     const long_description = req.body.long_description;
-//     const short_description = req.body.short_description;
-//     const genres = req.body.genres;
-//     const review_url = req.body.review_url;
-//     const review_score = req.body.review_score;
-    
-  
-//     db.query(
-//       "INSERT INTO code_foo.codefoobackend (name, age, country, position, wage) VALUES (?,?,?,?,?)",
-//       [name, age, country, position, wage],
-//       (err, result) => {
-//         if (err) {
-//           console.log(err);
-//         } else {
-//           res.send("Values Inserted");
-//         }
-//       }
-//     );
-// });
-
-
-// Get Image call 
+// Get Image call and store in cache
+// For Future Do API call and add all images to the database instead of cache
+// Fetch is in back end for scalibility of the web scraper and editing in case call needs to change
+const imageCache = {};
 app.get('/reviewfind/*', (req, res) => {
   var url = req.params[0];
   const base64Credentials = Buffer.from('avanbussum@gmail.com:0nZyrm8y0dAFhQOnS5wk').toString('base64')
@@ -68,17 +41,28 @@ app.get('/reviewfind/*', (req, res) => {
     }
   }
   console.log("The request for:" + url)
-  axios.get("https://api.urlmeta.org/?url=" + url, options).then(
+
+  if (imageCache[url]) {
+
+    res.send(imageCache[url])
+
+  } else {
+    axios.get("https://api.urlmeta.org/?url=" + url, options).then(
       (response) => {
         if (response.data.result.status == 'OK') {
-          res.send(response.data);
+          imageCache[url] = response.data.meta.image;
+          res.send(response.data.meta.image);
         } else {
           console.log(response.data.result.reason);
         }
       }
     );
+  }
 });
 
+app.get("/show-urlmeta-cache", (req, res) => {
+  res.send(imageCache);
+});
 
 
 app.get("/medias", (req, res) => {
@@ -128,8 +112,36 @@ app.get("/genrefinder/:genres", (req, res) => {
 });
 
 
-
 //localhost:3004
 app.listen(3004, () => {
     console.log("server is up and listening on 3004...");
 });
+
+// Insert media for future use if data needs to be changed from front end
+/*
+app.post("/create", (req, res) => {
+    
+    const id = req.body.id;
+    const media_type = req.body.media_type;
+    const name = req.body.name;
+    const short_name = req.body.short_name;
+    const long_description = req.body.long_description;
+    const short_description = req.body.short_description;
+    const genres = req.body.genres;
+    const review_url = req.body.review_url;
+    const review_score = req.body.review_score;
+    
+  
+    db.query(
+      "INSERT INTO code_foo.codefoobackend (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+      [name, age, country, position, wage],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send("Values Inserted");
+        }
+      }
+    );
+});
+*/
